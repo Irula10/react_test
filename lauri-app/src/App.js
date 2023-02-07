485157e96070ea97ab910c8059a03a64719e2d80
Julius <julius.karekallas@gmail.com>
2023-02-07 19:38:02 +0200

    message

---
+++ lauri-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import { Message } from "./pages/Message";
+
+function App() {
+  return (
+    <div>
+      <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ lauri-app/src/pages/Message.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Message = () => {
+    return (
+        <h1>Päivää</h1>
+    );
+}

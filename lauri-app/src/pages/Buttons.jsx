e83d680f6a43bef7284dfef12d2ef043f7c345a0
Irula10 <117756987+Irula10@users.noreply.github.com>
2023-03-07 18:56:34 +0200

    Update Buttons.jsx

--- lauri-app/src/pages/Buttons.jsx
+++ lauri-app/src/pages/Buttons.jsx
@@ -3,19 +3,26 @@ import { useState } from 'react';
 
 export const Buttons = () => {
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
 
     function countClics() {
-        setCount(count + 1);
+        setCount(count + (1 * count2));
+    }
+
+    function countClics2() {
+        setCount2(count2 + 0.1);
     }
 
     function resetCount() {
         setCount(0);
+        setCount2(0);
     }
 
     return (
         <div>
-            <button onClick={countClics}>Clicked: {count} times</button>
-            <button onClick={resetCount}>Clear count</button>
+            <button onClick={countClics}>Coins: {count}. Click to get more</button>
+            <button onClick={countClics2}>Coin multipalyer: {count2}. Click to get more</button>
+            <button onClick={resetCount}>Reset</button>
         </div>
     );
 }

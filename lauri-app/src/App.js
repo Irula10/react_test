cc0e2a00bed348d2a143dccb48198e5a7776d66a
Irula10 <117756987+Irula10@users.noreply.github.com>
2023-03-07 19:33:58 +0200

    Update App.js

--- lauri-app/src/App.js
+++ lauri-app/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Message } from "./pages/Message";
+import { Buttons } from "./pages/Buttons";
+import { Router } from "./pages/Router";
+
 
 function App() {
   return (
-    <div>
-      <Message />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route index path='/' element={<Router />}>
+            <Route index element={<Message />} />
+            <Route path='Buttons' element={<Buttons />} />
+          </Route>  
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
